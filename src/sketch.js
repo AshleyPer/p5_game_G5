c80f6a173d930a7c44d2fe4f2e12be1c70392018
Ashley <ashley.pergoliti@gmail.com>
2022-10-30 11:56:55 +1000

    Testing a branch..

--- src/sketch.js
+++ src/sketch.js
@@ -8,4 +8,6 @@ function draw() {
   text('This is not a test!', 300, height / 2)
   text("what will happen now", 300, height/2 +200)
   text('Testing the merge!', 300, height / 2)
+
+  text('Testing a branch!', 300, height / 2)
 }
